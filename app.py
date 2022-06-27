4807516615492f445fff34229a2e51bd30b44aa3
Anita Lee <anita.lee@gmail.com>
2022-06-27 11:39:33 -0400

    Prep heroku again

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from models import Student
 
 load_dotenv()
 
-# database_url = os.environ['DATABASE_URL']
-# database_url.replace('postgres://', 'postgresql://')
+database_url = os.environ['DATABASE_URL']
+database_url.replace('postgres://', 'postgresql://')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
