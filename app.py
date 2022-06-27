8508a1ba9e1fc49edcb4acd27f0405c975038b21
Anita Lee <anita.lee@gmail.com>
2022-06-27 12:42:27 -0400

    App works on local server'

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ from models import Student
 
 load_dotenv()
 
-database_url = os.environ['DATABASE_URL']
-database_url.replace('postgres://', 'postgresql://')
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=database_url
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
