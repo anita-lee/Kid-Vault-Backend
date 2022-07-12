4c823aff80eb81b90c83b70eba10c18a11f7c248
Anita Lee <anita.lee@gmail.com>
2022-07-12 09:35:16 -0400

    Fix user.patch route and contacts filter by student id route

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from models import User, db, connect_db, Student, MedicalRecord,DEFAULT_PROFILE_
 from datetime import timedelta
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
+# from forms import CSRFTokenForm
 
 load_dotenv()
-
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
@@ -34,6 +34,19 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
+# ##############################################################################
+# # CSRF Protection:
+
+# # @app.before_request
+# # def add_csrf_only_form():
+# #     """Add a CSRF-only form so that every route can use it"""
+
+# #     g.csrf_form = CSRFProtection()
+
+
+##############################################################################
+# JWT Protection:
+
 
 @jwt.user_identity_loader
 def user_identity_lookup(user):
@@ -55,10 +68,10 @@ def user_lookup_callback(_jwt_header, jwt_data):
     return User.query.filter_by(username=identity).one_or_none()
 
 
+##############################################################################
+# User Signup and Login:
 
-##### USER SIGN UP AND LOGIN #####
-
-@app.route('/signup', methods=["POST"])
+@app.post('/signup')
 @cross_origin()
 def signup():
     """Handle user signup.
@@ -80,12 +93,12 @@ def signup():
     except IntegrityError:
         return (jsonify({"error": "Duplicate Username/Email"}), 400)
 
-
     if user.username:
         token = create_access_token(identity=user.username)
         return (jsonify(token=token), 201)
 
-@app.route('/login', methods=["POST"])
+
+@app.post('/login')
 @cross_origin()
 def login():
     """ Handle user login and return a token """
@@ -102,110 +115,9 @@ def login():
     else:
         return (jsonify({"error": "Invalid Username/Password"}), 400)
 
-
-# import os
-# from flask import Flask, jsonify, redirect, request, session, g
-# from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
-# from flask_debugtoolbar import DebugToolbarExtension
-# from sqlalchemy.exc import IntegrityError
-# from dotenv import load_dotenv
-# from database import connect_db, db
-# from models import Student, Contact, MedicalRecord, User, GuardianChild
-# from datetime import timedelta
-# # from forms import CSRFProtection
-# # from flask_cors import CORS, cross_origin
-
-# load_dotenv()
-
-# app = Flask(__name__)
-
-# app.config["JWT_TOKEN_LOCATION"] = ["headers"]
-# app.config["JWT_COOKIE_SECURE"] = False
-# app.config["JWT_SECRET_KEY"] = os.environ['SECRET_KEY']
-# app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = (
-#     os.environ['DATABASE_URL'].replace('postgres://', 'postgresql://'))
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = False
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-# app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-
-# jwt = JWTManager(app)
-# toolbar = DebugToolbarExtension(app)
-
-# connect_db(app)
-
-
-# ##############################################################################
-# # User signup/login/logout
-
-# # @app.before_request
-# # def add_csrf_only_form():
-# #     """Add a CSRF-only form so that every route can use it"""
-
-# #     g.csrf_form = CSRFProtection()
-
-
 ##############################################################################
-# General user routes:
+# General User routes:
 
-
-@app.get("/")
-@cross_origin()
-def homepage():
-    """Show home."""
-
-    return redirect("/students")
-
-@app.get("/students")
-@cross_origin()
-def get_students():
-    """Get all students."""
-
-    students = Student.query.all()
-    return jsonify([student.serialize() for student in students])
-
-@app.get("/students/<int:id>")
-@cross_origin()
-def get_student(id):
-    """Get student by id."""
-
-    student = Student.query.get(id)
-    return jsonify(student.serialize())
-
-
-@app.get("/contacts")
-@cross_origin()
-def get_contacts():
-    """Get all contacts."""
-
-    contacts = Contact.query.all()
-    return jsonify([contact.serialize() for contact in contacts])
-
-@app.get("/contacts/<int:student_id>")
-@cross_origin()
-def get_contact(student_id):
-    """Get contacts by student id."""
-
-    contact = Contact.query.get(student_id)
-    return jsonify(contact.serialize())
-
-@app.get("/medicalrecords")
-@cross_origin()
-def get_medicalrecords():
-    """Get all medical records."""
-
-    medical_records = MedicalRecord.query.all()
-    return jsonify([medical_record.serialize() for medical_record in medical_records])
-
-@app.get("/medicalrecords/<int:student_id>")
-@cross_origin()
-def get_medicalrecord(student_id):
-    """Get medical records by student id."""
-
-    medical_record = MedicalRecord.query.get(student_id)
-    return jsonify(medical_record.serialize())
 
 @app.get("/users")
 @cross_origin()
@@ -215,56 +127,59 @@ def get_users():
     users = User.query.all()
     return jsonify([user.serialize() for user in users])
 
-@app.get("/guardianchildren")
+@app.patch('/users/<username>')
 @cross_origin()
-def get_guardianchildren():
-    """Get all guardian children. """
+@jwt_required()
 
-    guardian_children = GuardianChild.query.all()
-    return jsonify([guardian_child.serialize() for guardian_child in guardian_children])
+def update_user(username):
+    """Update a user's current info."""
 
-@app.get("/guardianchildren/<guardian_username>")
+    curr_user = get_jwt_identity()
+
+    if curr_user == username:
+
+        user = User.query.get_or_404(username)
+        user.first_name = request.json.get('first_name', user.first_name)
+        user.last_name = request.json.get('last_name', user.last_name)
+        user.email = request.json.get('email', user.email)
+        user.phone = request.json.get('phone', user.phone)
+
+        db.session.commit()
+        return (jsonify({"success": "user updated!"}), 200)
+
+    else:
+        return (jsonify({"error": "Unauthorized."}), 401)
+
+##############################################################################
+# General Student routes:
+
+
+@app.get("/")
 @cross_origin()
-def get_guardianchild(guardian_username):
-    """Get guardian children by guardian ."""
+def homepage():
+    """Show students."""
 
-    guardian_child = GuardianChild.query.get(guardian_username)
-    return jsonify(guardian_child.serialize())
+    return redirect("/students")
 
-@app.post("/guardianchildren")
+
+@app.get("/students")
 @cross_origin()
-def create_guardianchild():
-    """Create a new guardian child relationship."""
+def get_students():
+    """Get all students."""
 
-    guardian_child = GuardianChild(
-       guardian_username=request.json['guardian_username'],
-       child_id=request.json['child_id']
-    )
+    students = Student.query.all()
+    return jsonify([student.serialize() for student in students])
 
-    db.session.add(guardian_child)
-    db.session.commit()
-    return jsonify(guardian_child.serialize())
 
-# @app.post("/users")
-# @cross_origin()
-# def create_user():
-#     """Create a new user."""
+@app.get("/students/<int:id>")
+@cross_origin()
+def get_student(id):
+    """Get student by id."""
 
-#     user = User(
-#         username=request.json['username'],
-#         password=request.json['password'],
-#         first_name=request.json['first_name'],
-#         last_name=request.json['last_name'],
-#         email=request.json['email'],
-#         phone=request.json['phone'],
-#     )
-#     db.session.add(user)
-#     db.session.commit()
-#     return jsonify(user.serialize())
+    student = Student.query.get(id)
+    return jsonify(student.serialize())
 
-# npm
-
-@app.route('/students', methods=["POST"])
+@app.post('/students')
 @cross_origin()
 def create_student():
     """Create a new student."""
@@ -284,7 +199,7 @@ def create_student():
     except IntegrityError:
         return (jsonify({"error": "Duplicate Student"}), 400)
 
-@app.route('/students/<int:id>', methods=["DELETE"])
+@app.delete('/students/<int:id>')
 @cross_origin()
 def delete_student(id):
     """Delete a student."""
@@ -295,28 +210,83 @@ def delete_student(id):
 
     return (jsonify({"success" : "Student deleted."}), 200)
 
+##############################################################################
+# General Contacts routes:
 
-@app.route('/users/<username>', methods=['PATCH'])
+
+@app.get("/contacts")
 @cross_origin()
-# @jwt_required()
-def update_user(username):
-    """Update a user."""
+def get_contacts():
+    """Get all contacts."""
 
-    # curr_user = get_jwt_identity()
+    contacts = Contact.query.all()
+    return jsonify([contact.serialize() for contact in contacts])
 
-    # if curr_user == username:
 
-    user = User.query.filter_by(username=username).first()
+@app.get("/contacts/<int:student_id>")
+@cross_origin()
+def get_contact(student_id):
+    """Get contacts by student id."""
 
-    if user:
-        # user = User.query.get_or_404(username)
-        user.first_name = request.json['first_name'] or user.first_name,
-        user.last_name = request.json['last_name'] or user.last_name,
-        user.email = request.json['email'] or user.email,
-        user.phone = request.json['phone'] or user.phone,
+    contacts = Contact.get_by_id(student_id)
+    print("****************contact: ", contacts)
+    return jsonify([contact.serialize() for contact in contacts])
 
-        db.session.commit()
-        return (jsonify({"success": "user updated!"}), 200)
 
-    else:
-        return (jsonify({"error": "Unauthorized."}), 401)
+##############################################################################
+# General Medical Record routes:
+
+
+@app.get("/medicalrecords")
+@cross_origin()
+def get_medicalrecords():
+    """Get all medical records."""
+
+    medical_records = MedicalRecord.query.all()
+    return jsonify([medical_record.serialize() for medical_record in medical_records])
+
+
+@app.get("/medicalrecords/<int:student_id>")
+@cross_origin()
+def get_medicalrecord(student_id):
+    """Get medical records by student id."""
+
+    medical_record = MedicalRecord.query.get(student_id)
+    return jsonify(medical_record.serialize())
+
+
+##############################################################################
+# General GuardianChildren Record routes:
+
+
+@app.get("/guardianchildren")
+@cross_origin()
+def get_guardianchildren():
+    """Get all guardian children. """
+
+    guardian_children = GuardianChild.query.all()
+    return jsonify([guardian_child.serialize() for guardian_child in guardian_children])
+
+
+@app.get("/guardianchildren/<guardian_username>")
+@cross_origin()
+def get_guardianchild(guardian_username):
+    """Get guardian children by guardian ."""
+
+    guardian_child = GuardianChild.query.get(guardian_username)
+    return jsonify(guardian_child.serialize())
+
+
+@app.post("/guardianchildren")
+@cross_origin()
+def create_guardianchild():
+    """Create a new guardian child relationship."""
+
+    guardian_child = GuardianChild(
+       guardian_username=request.json['guardian_username'],
+       child_id=request.json['child_id']
+    )
+
+    db.session.add(guardian_child)
+    db.session.commit()
+    return jsonify(guardian_child.serialize())
